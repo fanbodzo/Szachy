c1111e252a7200ce214eab901d3009f1006eab53
Filip <82474139+fanbodzo@users.noreply.github.com>
2025-04-02 17:21:23 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    /*
+        Projekt gry w szachy , z obsluga wielu graczy oraz wielowatkowoscia
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

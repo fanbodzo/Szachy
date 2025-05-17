18831bf42faf97d85582b2ccd3afca472fbdd46d
filip <fanbodzo@gmail.com>
2025-05-17 22:31:16 +0200

    utworzenie fxml do logowania plus test commita z lapka

---
+++ src/view/controlers/LoginViewController.java
@@ -0,0 +1,16 @@
+package view.controlers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginViewController {
+    @FXML
+    private Pane bg;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
